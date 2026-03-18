7869b98181dc133a03aee6d8bd7b3f22ae8c80f7
NNAMDI C AMADI <nnamdiamadi1964@gmail.com>
2026-03-18 19:35:34 -0400

    Added key data exploration queries

--- sql/01_data_exploration.sql
+++ sql/01_data_exploration.sql
@@ -1,1 +1,20 @@
 -- SQL queries for data exploration
+-- Step 2.1: View Available Tables
+
+SELECT table_name
+FROM `bigquery-public-data.thelook_ecommerce.INFORMATION_SCHEMA.TABLES`
+ORDER BY table_name;
+
+
+-- Step 2.2: Inspect order_items Table
+
+SELECT *
+FROM `bigquery-public-data.thelook_ecommerce.order_items`
+LIMIT 10;
+
+
+-- Step 2.3: Inspect products Table
+
+SELECT *
+FROM `bigquery-public-data.thelook_ecommerce.products`
+LIMIT 10;
